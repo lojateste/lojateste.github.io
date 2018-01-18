e5729b614d8b405bd7799a79767821617bc8dc7d
JonatanPinheiro <jonatanpinheiro@outlook.com>
2018-01-18 09:47:23 -0300

    homepage incrementada

--- dist/js/home.js
+++ dist/js/home.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".owl-carousel.featured-items").owlCarousel({loop:!0,margin:10,dots:!0,autoplay:!0,autoplayTimeout:4e3,responsive:{0:{items:1},400:{items:2},600:{items:3},1200:{items:4}}})});
+$(document).ready(function(){responsiveCarousel={0:{items:1},400:{items:2},600:{items:3},1200:{items:4}},$(".owl-carousel.featured-items").owlCarousel({loop:!0,margin:10,dots:!0,autoplay:!0,autoplayTimeout:4e3,responsive:responsiveCarousel}),$(".owl-carousel.best-sellers").owlCarousel({loop:!0,margin:10,responsive:responsiveCarousel})});
--- src/js/home.js
+++ src/js/home.js
@@ -1,26 +1,37 @@
 $(document).ready(function() {
 
+  responsiveCarousel = {
+    0: {
+      items: 1
+    },
+    400: {
+      items: 2
+    },
+    600: {
+      items: 3
+    },
+    1200: {
+      items: 4
+    }
+
+  }
+
   // Gatihlos do Owl carousel
   $('.owl-carousel.featured-items').owlCarousel({
     loop: true,
     margin: 10,
     dots: true,
+    // nav: true,
     autoplay: true,
     autoplayTimeout: 4000,
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 2
-      },
-      600: {
-        items: 3
-      },
-      1200: {
-        items: 4
-      }
-    }
+    responsive: responsiveCarousel
+  })
+
+  $('.owl-carousel.best-sellers').owlCarousel({
+    loop: true,
+    margin: 10,
+    // nav: true,
+    responsive: responsiveCarousel
   })
 
 });
